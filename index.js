daff5021ac7decf2defbc70d39b942cec1997d8c
EStherMmutisya <starjacob994@gmail.com>
2024-01-15 13:55:17 +0300

    unfinisged assignment

--- index.js
+++ index.js
@@ -1,4 +1,57 @@
 /* Your Code Here */
+function createEmployeeRecord (firstName,familyName,title,payPerHour,timeInEvents,timeOutEvents){
+   // const[firstName,familyName, title,payPerHour]= employeeData
+
+    //const timeInEvents= []
+    //const timeOutEvents= []
+    
+
+    const employeeRecords =  {
+        firstName: "moe",
+        familyName: "Worm",
+        title: "Security",
+        payPerHour: 1,
+        timeInEvents: [],
+        timeOutEvents: [],
+    };
+    return employeeRecords;
+
+}
+function createEmployeeRecords(dataArray){
+    return dataArray.map(employeeData => createEmployeeRecord(...employeeData));
+
+}
+const employeeDataArray = [
+    ["moe", "bartholomew", "security", 1]
+   // ["matin", "mark", "Guide", 5]
+]
+const employeeRecordsArray = createEmployeeRecords(employeeDataArray)
+console.log(employeeRecord)
+
+function createTimeInEvent(employeeRecord, dateTimeString) {
+    if (!dateTimeString) {
+        throw new Error("Invalid dateTimeString");
+    }
+
+    const dateTimeArray = dateTimeString.split(" ");
+
+    if (dateTimeArray.length !== 2) {
+        throw new Error("Invalid dateTimeString format");
+    }
+
+    const [date, time] = dateTimeArray;
+    const hour = parseInt(time.split(":")[0], 10); // Extract hour correctly
+
+    const timeInEvent = {
+        type: "TimeIn",  // Correct type
+        date: date,
+        hour: hour,
+    };
+
+    employeeRecord.timeInEvents.push(timeInEvent);
+
+    return employeeRecord;
+}
 
 /*
  We're giving you this function. Take a look at it, you might see some usage
